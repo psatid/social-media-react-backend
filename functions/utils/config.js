3d8df5835d2e0b0899a977e0ce94dc0e8c0f5a96
psatid <47550184+psatid@users.noreply.github.com>
2020-05-27 23:33:58 +0700

    Delete config.js

--- functions/utils/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyBE74zY-x9tCxm1sTzB-WZD92GiKR1nvxk",
-    authDomain: "hw-project-1537196375025.firebaseapp.com",
-    databaseURL: "https://hw-project-1537196375025.firebaseio.com",
-    projectId: "hw-project-1537196375025",
-    storageBucket: "hw-project-1537196375025.appspot.com",
-    messagingSenderId: "821816057450",
-    appId: "1:821816057450:web:e5c93d343c4f62d848f413"
-};
